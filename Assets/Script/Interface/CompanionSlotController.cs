ffd69ec7d00f048e3a4952ceb5795e7491cc5263
river-minj <hanr221@gmail.com>
2026-02-03 18:06:23 +0900

    리스트 슬롯 리소스 스파인 추가 베이스

--- Assets/Script/Interface/CompanionSlotController.cs
+++ Assets/Script/Interface/CompanionSlotController.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using Spine.Unity;
+using TMPro;
 using UnityEngine;
 
 
@@ -9,13 +10,21 @@ public class CompanionSlotData : InfiniteScrollData
 
 public class CompanionSlotController : InfiniteScrollItem
 {
+	[Header("UI")]
 	[SerializeField]  private TextMeshProUGUI _nameText;
+	[SerializeField] private SkeletonGraphic _spine;
+
+	[Header("Animation Setting")]
+	[SerializeField] private string _idleAnimationName = "Idle";
+	[SerializeField] private bool _pauseInvisible = true;//안보일때 애니메이션 정지여부
+
+	private bool _isVisible = false;
 
 	protected override void RefreshUI()
 	{
-		if(_data is CompanionSlotData companionData)
+		if(_data is CompanionSlotData slotData)
 		{
-			if(companionData._companionData == null)
+			if(slotData._companionData == null)
 			{
 				Debug.LogError("CompanionSlotData의 CompanionData가 null입니다.");
 				return;
@@ -23,9 +32,26 @@ public class CompanionSlotController : InfiniteScrollItem
 			
 			if (_nameText != null)
 			{
-				_nameText.text = companionData._companionData._companionName;
+				_nameText.text = slotData._companionData._companionName;
 			}
-
+			
+			if(_spine != null)
+			{
+				SetUpSpine(slotData._companionData);
+			}
 		}
+	}
+
+	private void SetUpSpine(CompanionData data)
+	{
+		//if(_spine == null || data == null || data._skeletonDataAsset == null)
+		{
+			Debug.LogError("스파인 설정에 필요한 데이터가 누락되었습니다.");
+			return;
+		}
+
+		//_spine.skeletonDataAsset = data._skeletonDataAsset;
+		_spine.Initialize(true);
+		_spine.AnimationState.SetAnimation(0, _idleAnimationName, true);
 	}
 }
