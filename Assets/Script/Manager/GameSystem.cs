4e17dc166ab38e9a4e6be8ffc5d6dc4884fb048d
river-minj <hanr221@gmail.com>
2026-02-25 14:21:10 +0900

    게임 데이터 세이브 파일 생성

---
+++ Assets/Script/Manager/GameSystem.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+
+[Serializable]
+public class  SaveData
+{
+	public PlayerInfoSaveData playerInfo;
+	public QuestSaveData questInfo;
+}
+
+[Serializable]
+public class PlayerInfoSaveData
+{
+	public string name;
+	public int level;
+	public int exp;
+	public int gold;
+}
+
+[Serializable]
+public class QuestSaveData
+{
+	public List<string> startedQuests = new List<string>();
+	public List<string> completedQuests = new List<string>();
+	public List<ActiveQuestEntry> activeQuests = new List<ActiveQuestEntry>();
+}
+
+[Serializable]
+public class ActiveQuestEntry
+{
+	public string questID;
+	public int currentProgress;
+	public bool isCompleted;
+}
+
+public class GameSystem : MonoBehaviour
+{
+	private static string SavePath => Path.Combine(Application.persistentDataPath, "saveData.json");
+
+	private static bool Exists() => File.Exists(SavePath);
+
+	public static void Save(SaveData data)
+	{
+		string json = JsonUtility.ToJson(data, true);
+		File.WriteAllText(SavePath, json);
+		Debug.Log($"Game saved to {SavePath}");
+	}
+
+	public static SaveData Load()
+	{
+		if (!File.Exists(SavePath))
+		{
+			Debug.LogWarning($"No save file found at {SavePath}");
+			return null;
+		}
+
+		string json = File.ReadAllText(SavePath);
+		SaveData data = JsonUtility.FromJson<SaveData>(json);
+		Debug.Log($"Game loaded from {SavePath}");
+		return data;
+	}
+
+	public static void Delete()
+	{
+		if (File.Exists(SavePath))
+		{
+			File.Delete(SavePath);
+			Debug.Log($"Save file deleted at {SavePath}");
+		}
+		else
+		{
+			Debug.LogWarning($"No save file to delete at {SavePath}");
+		}
+	}
+}
