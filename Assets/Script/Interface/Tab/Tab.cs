d635e0a87f8c91efe7348c47013e78c45b567e95
river-minj <hanr221@gmail.com>
2026-05-01 14:55:31 +0900

    탭 상태별 확장

--- Assets/Script/Interface/Tab/Tab.cs
+++ Assets/Script/Interface/Tab/Tab.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using static TabController;
 
 public class Tab : MonoBehaviour
 {
@@ -7,7 +8,84 @@ public class Tab : MonoBehaviour
 	[SerializeField] private Button _tabButton;
 	[SerializeField] private TabPage _linkedPage;
 
+	[Header("Visual")]
+	[SerializeField] private Image _image;
+	[SerializeField] private Sprite _selectedSprite;
+	[SerializeField] private Sprite _normalSprite;
+	[SerializeField] private Sprite _lockedSprite;
+
+	[Header("Initial State")]
+	[SerializeField] private bool _startLocked = false;
+
 	public Button TabButton => _tabButton;
 	public TabPage LinkedPage => _linkedPage;
+	private TabState _state = TabState.Normal;
+	public TabState State => _state;
+	public bool IsLocked => _state == TabState.Locked;
+
+	private void Awake()
+	{
+		if (_startLocked)
+		{
+			SetLocked(true);
+		}
+		else
+		{
+			ApplyState(TabState.Normal);
+		}
+	}
+	public void SetSelected(bool selected)
+	{
+		// 잠긴 탭은 선택 상태로 못 바뀜
+		if (_state == TabState.Locked)
+		{
+			return;
+		}
+
+		ApplyState(selected ? TabState.Selected : TabState.Normal);
+	}
+
+	public void SetLocked(bool locked)
+	{
+		if (locked)
+		{
+			ApplyState(TabState.Locked);
+		}
+		else
+		{
+			// 잠금 해제 시 기본은 Normal
+			ApplyState(TabState.Normal);
+		}
+	}
+
+	private void ApplyState(TabState newState)
+	{
+		_state = newState;
+
+		// 클릭 가능 여부
+		if (_tabButton != null)
+		{
+			_tabButton.interactable = (newState != TabState.Locked);
+		}
+
+		// 스프라이트 갱신
+		if (_image == null)
+		{
+			return;
+		}
+
+		switch (newState)
+		{
+			case TabState.Selected:
+				_image.sprite = _selectedSprite;
+				break;
+			case TabState.Normal:
+				_image.sprite = _normalSprite;
+				break;
+			case TabState.Locked:
+				_image.sprite = _lockedSprite;
+				break;
+		}
+	}
 }
 
--- Assets/Script/Interface/Tab/TabController.cs
+++ Assets/Script/Interface/Tab/TabController.cs
@@ -8,36 +8,56 @@ public class TabController : MonoBehaviour
 	private List<Tab> _tabs = new List<Tab>();
 	public event Action<Tab> OnTabChanged;
 
+	public enum TabState
+	{
+		Normal,
+		Selected,
+		Locked,
+	}
+
 	private void Awake()
 	{
 		_tabs = new List<Tab>(GetComponentsInChildren<Tab>(true));
 		foreach (var tab in _tabs)
 		{
 			var captured = tab;
+			//자식으로 있는 모든 탭 버튼에 이벤트 연결
 			captured.TabButton.onClick.AddListener(() => ChangeTab(captured));
 		}
 	}
 
 	public void SetDefaultTab()
 	{
-		if(_tabs.Count >0)
+		// 첫 번째 잠기지 않은 탭을 선택
+		foreach (var tab in _tabs)
 		{
-			ChangeTab(_tabs[0]);
+			if (!tab.IsLocked)
+			{
+				ChangeTab(tab);
+				return;
+			}
 		}
 	}
 
 	private void ChangeTab(Tab selectedTab)
 	{
+		//잠긴 탭은 선택 불가
+		if (selectedTab.IsLocked)
+		{
+			return;
+		}
+
 		HashSet<GameObject> activePages = new HashSet<GameObject>();
 
 		// 선택된 탭의 페이지를 먼저 수집
 		activePages.Add(selectedTab.LinkedPage.gameObject);
 
-		// 수집된 페이지만 활성화, 나머지 비활성화
+		// 수집된 페이지만 활성화, 나머지 비활성화 + 탭 비주얼 갱신
 		foreach (var tab in _tabs)
 		{
 			bool shouldActive = activePages.Contains(tab.LinkedPage.gameObject);
 			tab.LinkedPage.gameObject.SetActive(shouldActive);
+			tab.SetSelected(tab == selectedTab);
 		}
 
 		OnTabChanged?.Invoke(selectedTab);
